12ab4250770ba4242525c9a0b7257f86ffba15f8
John Bloom <john@sheeplauncher.net>
2025-04-20 23:35:48 -0700

    WIP: Very WIP ST77916 driver.

---
+++ api_drivers/common_api_drivers/display/st77916/st77916.py
@@ -0,0 +1,12 @@
+from micropython import const  # NOQA
+import display_driver_framework
+
+STATE_HIGH = display_driver_framework.STATE_HIGH
+STATE_LOW = display_driver_framework.STATE_LOW
+STATE_PWM = display_driver_framework.STATE_PWM
+
+BYTE_ORDER_RGB = display_driver_framework.BYTE_ORDER_RGB
+BYTE_ORDER_BGR = display_driver_framework.BYTE_ORDER_BGR
+
+class ST77916(display_driver_framework.DisplayDriver):
+    pass
